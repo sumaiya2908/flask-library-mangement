a4b35544b8e66e27428912e4cc0e0a5c8dbfbfa2
sumaiya2908 <58825865+sumaiya2908@users.noreply.github.com>
2021-05-24 12:13:27 +0530

    set up db URI

--- library/__init__.py
+++ library/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2f82262027aa46e09328b96c' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fcnxppipqwfwxz:cb775483628b0031ad9a448dd1ab159a580408c7a7fa63cca0f395e4b4ac9c97@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d4he96qecvdttk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fcnxppipqwfwxz:cb775483628b0031ad9a448dd1ab159a580408c7a7fa63cca0f395e4b4ac9c97@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d4he96qecvdttk'
 db = SQLAlchemy(app)
 
 
