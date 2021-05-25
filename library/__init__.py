e544a2442d5ee47207e9b7dd6f8a74b7f625d8f4
sumaiya2908 <58825865+sumaiya2908@users.noreply.github.com>
2021-05-25 23:44:46 +0530

    fix production url bug

--- library/__init__.py
+++ library/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
