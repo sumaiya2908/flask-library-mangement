2f538253e54d534c61496ae710e89298d8415614
sumaiya2908 <58825865+sumaiya2908@users.noreply.github.com>
2021-05-26 10:34:07 +0530

    add screenshots

--- library/__init__.py
+++ library/__init__.py
@@ -5,10 +5,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+ENV ='production'
 
+if ENV == 'dev':
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SECRET_KEY'] = ''
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
