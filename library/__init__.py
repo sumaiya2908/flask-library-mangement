0eaf6021c170893108b908292d22fd0a7f0027cd
sumaiya2908 <58825865+sumaiya2908@users.noreply.github.com>
2021-05-26 12:23:00 +0530

    set DB URL for local run

--- library/__init__.py
+++ library/__init__.py
@@ -5,10 +5,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-ENV ='production'
+ENV ='dev'
 
 if ENV == 'dev':
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
     app.config['SECRET_KEY'] = ''
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']
@@ -16,6 +16,5 @@ else:
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 from library.routes import routes, book_routes, member_routes, transaction_routes 
